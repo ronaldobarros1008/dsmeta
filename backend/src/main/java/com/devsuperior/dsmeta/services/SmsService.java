ad5dc044da9d23bf96661344a0b6f1a47b52da1e
ronaldobarros1008 <ronaldobarros1008@gmail.com>
2022-09-10 10:05:10 -0300

    Axios, useEffect first request

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -36,7 +36,7 @@ public class SmsService {
 		
 		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date
 				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
-
+		
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
